ccdede9182d0c2439cd6c2a6ac13fc7d379fb219
igorcarmona-git <igorcarmona1@hotmail.com>
2026-06-01 19:16:08 -0300

    fix: calculation of round tip and fix warning project

--- app/src/main/java/com/example/tiptimeapp/MainActivity.kt
+++ app/src/main/java/com/example/tiptimeapp/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.tiptimeapp
 
-import android.icu.number.NumberFormatter
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -12,46 +12,52 @@ import java.text.NumberFormat
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.main)
+
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-//        binding.tipResult.text = "0.00"
-//        binding.calculateButton.setOnClickListener {
-//            calculateTip()
-//        }
+        binding.tipResult.text = "Tip Amount:"
+        binding.btCalculate.setOnClickListener {
+            calculateTip()
+        }
     }
 
-//    private fun calculateTip() {
-//        // entries
-//        val stringInTextField = binding.costOfService.text.toString()
-//        val cost = stringInTextField.toDoubleOrNull() ?: return
-//
-//        val selectedID = binding.tipOption.checkedRadioButtonId
-//        val isRoundUp = binding.roundUpSwitch.isChecked
-//
-//        // process
-//        val tipPercentage = when (selectedID) {
-//            R.id.option_twenty_percent -> 0.20
-//            R.id.option_eighteen_percent -> 0.18
-//            else -> 0.15
-//        }
-//
-//        var tip = cost * tipPercentage
-//
-//        if(isRoundUp) {
-//            tip = kotlin.math.ceil(tip)
-//        }
-//
-//        // output
-//        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
-//        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
-//    }
+    private fun calculateTip() {
+        // entries
+        val stringInTextField = binding.etCostOfServiceInput.text.toString()
+
+        // ----------- Check if value is Null, if null return "?:" operator -----------------
+        val cost = stringInTextField.toDoubleOrNull() ?: return
+
+        val selectedID = binding.tipOptions.checkedRadioButtonId
+        val isRoundUp = binding.switchRoundUp.isChecked
+
+        // process
+        val tipPercentage = when (selectedID) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        var tip = cost * tipPercentage
+
+        if(isRoundUp) {
+            tip = kotlin.math.ceil(tip)
+        }
+
+        // output
+        // --------------  Format the local currency of the country set on device --------------
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
 }
